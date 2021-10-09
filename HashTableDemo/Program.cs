9f09dea8b5eddd45db7df1ab40bfa2ec863fd3b3
Mabel Christina <maybelchristina@gmail.com>
2021-10-09 07:54:26 +0530

    [Mabel] Add:UC1 added

---
+++ HashTableDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HashTableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MapDemo<string, string> hash = new MapDemo<string, string>(6);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value: " + hash5);
+
+        }
+    }
+}
